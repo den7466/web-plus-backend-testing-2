35539617044437e72ae7f7d16d2c1c214ee7f38e
Dmitriy Podorozhnykh <veseliy.koteika@yandex.ru>
2024-12-10 14:57:29 +0300

    feat: add findMany test

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,4 +1,4 @@
-import { PostsService } from './posts.service';
+import { Post, PostsService } from './posts.service';
 
 describe('PostsService', () => {
   let postsService: PostsService;
@@ -8,6 +8,7 @@ describe('PostsService', () => {
   });
 
   describe('.findMany', () => {
+    let mockedPostsWithId: Post[] = [];
     const posts = [
       {text: 'Post 1'},
       {text: 'Post 2'},
@@ -17,16 +18,23 @@ describe('PostsService', () => {
 
     beforeEach(() => {
       posts.forEach((post) => postsService.create(post));
+      let id = 1;
+      mockedPostsWithId = posts.map(item => {
+        const stringId = id.toString();
+        const postWithId = {...item, id: stringId};
+        id++;
+        return postWithId;
+      });
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      expect(postsService.findMany()).toEqual(mockedPostsWithId);
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const sliceData = mockedPostsWithId.slice(1);
+      const limitData = sliceData.slice(0, 2);
+      expect(postsService.findMany({ skip: 1, limit: 2 })).toEqual(limitData);
     });
-
-    // реализуйте недостающие тест-кейсы
   });
 });
